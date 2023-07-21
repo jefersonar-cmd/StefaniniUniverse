ce26ed63cd11244fde050a9c550209938b6889a0
Jefferson Santos Araújo <71133431+jefersonar-cmd@users.noreply.github.com>
2023-07-21 02:54:07 -0300

    quase finalizado
    aplicação faltante Jogo de Adivinhação

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,21 @@
 $(window).on('load', () => {
-    $('#conteudo').load('./src/cotacao.html')
+    $('#conteudo').load('./src/adivinha.html')
     $('#home').on('click', () => {
         $('#conteudo').load('./src/home.html')
     })
     $('#tabu').on('click', () => {
         $('#conteudo').load('./src/tabuada.html')
+    })
+    $('#moeda').on('click', () => {
+        $('#conteudo').load('./src/cotacao.html')
+    })
+    $('#sal').on('click', () => {
+        $('#conteudo').load('./src/salario.html')
+    })
+    $('#imc').on('click', () => {
+        $('#conteudo').load('./src/imc.html')
+    })
+    $('#game').on('click', () => {
+        $('#conteudo').load('./src/adivinha.html')
     })
 })
